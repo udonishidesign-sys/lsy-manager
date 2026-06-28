35d234050e1f40cc96d5e62f6e730a055e7f2537
udonishidesign-sys <udonishi.design@gmail.com>
2026-06-29 03:52:10 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "192.168.11.24",
-  ],
-};
-
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
